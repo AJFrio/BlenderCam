e4fed5ac5abc228d0a0fe5a1b2f79f690674f984
AJFrio <af4family@gmail.com>
2023-09-12 09:53:35 -0600

    Added documentation to functions

--- main.py
+++ main.py
@@ -11,14 +11,26 @@ def render(renderNumber):
     bpy.ops.render.render(write_still = True)
 
 #Y must be greater than 0, it will be the radius of the circle
-def center(y, z):
+def center(y: float, z: float) -> None:
+    '''Centers the camera on the x axis and moves to y and z coordinates
+    params:
+        y: Y coordinate of the camera
+        z: Z coordinate of the camera
+    returns:
+        None
+    '''
     cam.location.x = 0
     cam.location.y = y
     cam.location.z = z
     cam.rotation_euler[2] = cam.rotation_euler[2] + math.radians(0)
     cam.rotation_euler[1] = cam.rotation_euler[1] + math.radians(0)
 
-def EPR():
+def EPR() -> None:
+    '''
+    Camera orbits at a distance of y set in center() and takes 8 renders 
+    returns:
+        None
+    '''
     r = cam.location.y
     render(0)
     for i in range(8):
@@ -27,5 +39,7 @@ def EPR():
         cam.rotation_euler[2] = cam.rotation_euler[2] + math.radians(-45)
         render(i + 1)
         
+
+#Coordinates for personal test
 center(-20.37, 12.5)
 EPR()
